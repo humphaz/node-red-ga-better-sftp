0228376401d3d3426d2e24cbf131b1bde92c36ff
Glyn Humphreys <glyn@galaarch.com>
2021-09-28 15:53:30 +0000

    Updated index.js

--- transports/sftp/index.js
+++ transports/sftp/index.js
@@ -13,6 +13,7 @@ module.exports = function (RED) {
 	  this.port = config.port;
 	  this.username = config.username;
 	  this.password = config.password;
+      this.connected_ftp_source = null;
     };
 
     function SFtpNode(n) {
@@ -96,7 +97,17 @@ module.exports = function (RED) {
                 }
             }
 
-            let sftp = new Client();
+            let sftp;
+
+            if ( node.sftpConfig.credentials.connected_ftp_source === null)
+`           {
+                sftp = new Client();
+            }
+            else
+            {
+                sftp = node.sftpConfig.credentials.connected_ftp_source;
+            }
+            
             node.status({ fill: "blue", shape: "dot", text: 'setup' });
             try {
                 node.workdir = msg.workdir || node.workdir || "./";
